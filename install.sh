c6b6e8bfb01c27d476434c6e8d8070c3cf34fee1
Flavius Fiat <flaviusioan.fiat@gmail.com>
2026-03-06 00:08:25 +0200

    Fix install permissions

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 echo "Downloading IFF compiler for $OS..."
-curl -L "https://github.com/Flaviuz1/iff/releases/latest/download/iff-$OS" -o /usr/local/bin/iff
-chmod +x /usr/local/bin/iff
+curl -L "https://github.com/Flaviuz1/IFF/releases/latest/download/iff-Linux" -o /tmp/iff
+sudo mv /tmp/iff /usr/local/bin/iff
+sudo chmod +x /usr/local/bin/iff
 echo "Done! Run 'iff yourfile.iff' to get started."
